d5b151d36d65f9ee9f1e0148943c1c217ab3d2d5
Richard H <richard@haddads.net.au>
2022-12-13 19:58:27 +1000

    build: setup tabs and screens

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./assets/screens/HomeScreen";
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Tabs.Navigator>
+        <Tabs.Screen name={"Home"} component={HomeScreen} />
+        <Tabs.Screen name={"List"} component={HomeScreen} />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 }
---
+++ assets/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
